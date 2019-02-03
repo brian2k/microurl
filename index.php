9f321857aaee9b613f9b9de4afdc5245c77ed046
brian2k <baris.sarial@gmail.com>
2019-02-03 21:55:13 +0100

    Add files via upload
    responsive css and dynamic urls

--- index.php
+++ index.php
@@ -5,9 +5,10 @@ require 'crypto_token.php';
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$pure_url = explode('?', $actual_link);
 
-$maxi_url = $_POST['url'];
-$micro_url = $_GET['u'];
+$maxi_url = filter_input(INPUT_POST, 'url');
+$micro_url = filteR_input(INPUT_GET,'u');
 
 // GENERATE RANDOM NUMBER FOR BACKGROUND-IMAGE
 $count_files = count(scandir('bg')) - 3;
@@ -23,7 +24,7 @@ if($micro_url  && substr($actual_link, -1) != '-'){
 
     if ($result->num_rows > 0) {
         
-        // FIRST ADD A CLICK TO THE COUNTER BY UPDATING THE ROW
+        // FIRST ADD A CLICK TO THE COUNTER BY UPDrATING THE ROW
         $conn->query("UPDATE microURL SET count = '" . ++$row['count']. "' WHERE id = '" . $row['id'] . "'");
         // REDIRECT TO URL
         $redirect_url = $row['maxi_url'];
@@ -43,12 +44,12 @@ if($micro_url  && substr($actual_link, -1) != '-'){
 <html>
 <head>
     <title>microURL - Open Source</title>
-    <link rel="stylesheet" type="text/css" href="main.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
     <meta name="viewport" content="user-scalable=no" />
 </head>
 <body style="background: url(bg/bg_<?php echo $rand_image ?>.jpg) no-repeat center center fixed;   background-size: cover;">
     <div class="wrapper">
-        <div class="welcome">URL micromizer</div>
+        <div class="welcome"><span>URL micromizer</span></div>
         <div class="form">
             <form action="index.php" method="post">
                 <input type="text" class="url" name="url" value="<?php echo $maxi_url  ?>" placeholder="Paste your URL here">
@@ -72,13 +73,25 @@ if($micro_url  && substr($actual_link, -1) != '-'){
             $micro_url = substr_replace($micro_url ,"",-1);
 
             // LOOK IF THERE IS ANY URL WITH THAT TOKEN
-            $result = $conn->query("SELECT id, micro_url, maxi_url, count FROM microURL WHERE micro_url LIKE '" . $micro_url . "'");
+            $result = $conn->query("SELECT id, micro_url, maxi_url, count, date FROM microURL WHERE micro_url LIKE '" . $micro_url . "'");
             $row = $result->fetch_assoc();
 
             if ($result->num_rows > 0) {
-            echo "<span class=\"stats\">statistics:<br>your URL&nbsp;" . $row['maxi_url'] . "<br>";
-                echo "shortcode:&nbsp;<a href='http://microurl.crosscreations.de?u=" . $row['micro_url'] . "'>http://microurl.crosscreations.de?u=" . $row['micro_url'] . "</a><br>";
-                echo "clicked&nbsp;" . (int) $row['count'] . "x<br></span>";
+                
+                
+            echo "<span class='stats'><p class='caption'>statistics:</p>" 
+                . date("d.m.Y H:i:s",strtotime($row['date']));
+            
+                echo "<p class='subs'>your URL from:</p><p class='maxi_url'>" . $row['maxi_url'] . "</p>";
+            
+                echo "<p class='subs'>shortcode:</p><a href='" . $pure_url[0] . "?u=" . $row['micro_url'] . "'>" . $pure_url[0] . "?u=" . $row['micro_url'] . "</a>";
+
+                if(!(int) $row['count']){
+                    echo "<p class='subs'>Sorry! No clicks yet..</p></span>";
+                }
+                else{
+                    echo "<p class='subs'>" . (int) $row['count'] .  " clicks</p></span>";
+                }
             }        
         }
 
@@ -100,13 +113,13 @@ if($micro_url  && substr($actual_link, -1) != '-'){
                     } 
                     else{        
                         $micro_url = getToken(6);   // CREATE UNIQUE MICRO-URL
-                        $expire = $_POST['expire'];
+//                        $expire = $_POST['expire'];
                         //  INSERT INTO TABLE
             //            $sql = "INSERT INTO microURL (maxi_url, micro_url, exp) VALUES ('" . $maxi_url . "', '" . $micro_url . "', '" . $expire ."')";
                         $sql = "INSERT INTO microURL (maxi_url, micro_url) VALUES ('" . $maxi_url . "', '" . $micro_url . "')";
 
                         if ($conn->query($sql) === TRUE) {
-                            echo "your URL:&nbsp;<a href='http://microurl.crosscreations.de?u=" . $micro_url . "' class'micro_url'>http://microurl.crosscreations.de?u=" . $micro_url . "</a>";
+                            echo "<span>your URL:&nbsp;<a href='" . $pure_url[0] . "?u=" . $micro_url . "' class'micro_url'>" . $pure_url[0] . "?u=" . $micro_url . "</a></span>";
                         } else {
                             echo "error: " . $conn->error;
                         }
